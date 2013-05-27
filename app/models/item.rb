c5d2d95d88719497dd3bcf47956b8193ffe8bca3
Secretariat1987 <mixey.viti@bigmir.net>
2013-05-28 00:00:20 +0300

    colors and sizes added

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :brand
 	has_one :description
+  has_and_belongs_to_many :colors
+  has_and_belongs_to_many :sizes
 
 
 	self.per_page = 40
---
+++ app/models/size.rb
@@ -0,0 +1,8 @@
+class Size< ActiveRecord::Base
+
+	 has_and_belongs_to_many :items
+
+	 attr_accessible	:size_value,
+      							:size_value_ru
+
+end
