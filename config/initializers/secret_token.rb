3ac38b034964cc635825a064fa6516cbe3ab7ea8
Secretariat1987 <mixey.viti@bigmir.net>
2013-07-02 09:20:14 +0300

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garderob4ik::Application.config.secret_token = '6be6eb03ede9c5deafa766384ac6d156835c71c4607673c285fd3f34524563fc3e43f7e72f44926ca9444306eade971236163d403cb455ca23296b807de6d014'
