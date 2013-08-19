1d0f5dfd46a82d967e9383553a689070119194de
«Secretariat1987» <mixey.viti@bigmir.net>
2013-08-19 22:38:34 +0300

    add target blank

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
 	belongs_to :category
   belongs_to :brand
 	belongs_to :width
+  belongs_to :style
+  belongs_to :material
 	has_one :description
   has_and_belongs_to_many :colors
   has_and_belongs_to_many :sizes
