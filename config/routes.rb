fadbae5c4bf463bb7af35e171d2df2e0f329fb97
«Secretariat1987» <mixey.viti@bigmir.net>
2013-10-05 18:19:08 +0300

    items controller started

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Garderob4ik::Application.routes.draw do
 
   root :to => 'page#index'
 
+  get "page/index"
+  get "gard/all"
+  get "brands/list"
+
   get "/about_us" => "page#show", :uri=>'about_us'
 
   resources :gard, only: [ :all, :show ]
   resources :brands, only: [ :show, :list ]
   resources :category, only: [ :show ]
+  resources :material, only: [ :show ]
   resources :style, only: [ :show ]
   resources :width, only: [ :show ]
   resources :color, only: [ :show ]
@@ -19,6 +24,7 @@ Garderob4ik::Application.routes.draw do
     resources :category
     resources :istyle
     resources :color
+    resources :items
 
     resources :brands do
       resources :items do
