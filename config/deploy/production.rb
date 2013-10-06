f319da990a26504aef348c97f9e415027656df29
«Secretariat1987» <mixey.viti@bigmir.net>
2013-10-06 18:10:33 +0300

    setup shared public

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ namespace :deploy do
 
   task :symlink_shared do
     # run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/uploads #{release_path}/public/images"
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public"
   end
 
   task :bundle do
