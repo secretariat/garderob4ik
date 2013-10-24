b5604aab9c6c840bb255ce1515015c21a6214857
«Secretariat1987» <mixey.viti@bigmir.net>
2013-10-25 01:05:09 +0300

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,13 +6,13 @@ Garderob4ik::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
